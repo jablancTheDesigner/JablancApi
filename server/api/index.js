713a405aefdcaa49a9d7af55f80f009f97f386a8
Jahmal Blanchard <jablancdesigns@gmail.com>
2025-04-22 07:48:47 -0500

    Aloolist update

--- server/api/index.js
+++ server/api/index.js
@@ -12,24 +12,21 @@ const port = process.env.port || 5050;
 const __dirname = path.resolve();
 
 const whitelist = ['http://jablanc.com', "https://api-jablanc.vercel.app"]
-const corsOptions = {
-    methods: 'GET,POST',
-    allowedHeaders: 'Content-Type',
-    origin: function (origin, callback) {
-        console.log("origin =====>", origin);
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
+var corsOptionsDelegate = function (req, callback) {
+    var corsOptions;
+    if (whitelist.indexOf(req.header('Origin')) !== -1) {
+      corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    } else {
+      corsOptions = { origin: false } // disable CORS for this request
     }
-}
+    callback(null, corsOptions) // callback expects two parameters: error and options
+  }
 
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, 'dashboard/dist')));
 
-app.use("/projects", cors(corsOptions), projectsRouter)
+app.use("/projects", cors(corsOptionsDelegate), projectsRouter)
 
 app.listen(port , (err) => {
     console.log(`Server on port localhost:${port}`)
